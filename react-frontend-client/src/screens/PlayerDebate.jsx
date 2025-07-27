bb9a95060406e966e43e056ab9a9a8ca0ff1a967
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2025-07-27 16:52:19 +0300

    player debate screen added

--- react-frontend-client/src/screens/PlayerDebate.jsx
+++ react-frontend-client/src/screens/PlayerDebate.jsx
@@ -1,7 +1,20 @@
+import { PlayerImg } from "../misc/PlayerImg";
+import { sendDebaterPreference } from "../ServerComm";
+
 export default function PlayerDebate( {debaters}) {
   return (
     <div className="textbox">
-      <h2>TODO!</h2>
+      <h2>במי אתם תומכים?</h2>
+      <div css={css`display:flex; flex-direction: column; gap: 0.5em;`}>
+        {debaters.map((playerNumber) => {
+            return <button onClick={sendDebaterPreference(playerNumber)}>
+                <PlayerImg playerNumber={playerNumber} />
+            </button>
+        })}
+        <button onClick={sendDebaterPreference(-1)}>
+                אני לא תומכ.ת בשניהם!
+            </button>
+      </div>
     </div>
   );
 }
